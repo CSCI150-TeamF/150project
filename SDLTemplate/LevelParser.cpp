53f2c4c5433d508dbfc95daf89d2a9153e9bb369
Jlgarcia12 <jlgarcia_27@hotmail.com>
2014-10-22 16:17:35 -0700

    Added parseTileLayer in LevelParser(Needs zlib)

--- SDLTemplate/LevelParser.cpp
+++ SDLTemplate/LevelParser.cpp
@@ -1,4 +1,5 @@
 #include "LevelParser.h"
+#include "Base64.h"
 #include "includes.h"
 /* //Needs TiXmlDocument in order to Run correctly
 
@@ -59,4 +60,53 @@ void LevelParser::parseTilesets(TiXmlElement* pTilesetRoot,vector<Tileset>* pTil
 	tileset.numColumns = tileset.width / (tileset.tileWidth + tileset.spacing);
 
 	pTilesets->push_back(tileset);
+}
+
+void LevelParser::parseTileLayer(TiXmlElement* pTileElement, vector<Layer*> *pLayers, const vector<Tileset>* pTilesets)
+{
+	TileLayer* pTileLayer = new TileLayer(m_Tilesize, *pTilesets);
+
+	//data of the tile
+	vector<vector<int>> data;
+	string decodedIDs;
+	TiXmlElement* pDataNode;
+
+	for (TiXmlElement* e = pTileElement->FirstChildElement(); e != NULL; e = e->NextSiblingElement())
+	{
+		if (e->Value() == string("data"))
+		{
+			pDataNode = e;
+		}
+	}
+
+	for (TiXmlNode* e = pDataNode->FirstChild(); e != NULL; e = e->NextSibling())
+	{
+		TiXmlText* text = e->ToText();
+		string t = text->Value();
+		decodedIDs = base64_decode(t);
+	}
+	//zlib needed for functions
+
+	uLongf numGids = m_width * m_height * sizeof(int);
+	vector<unsigned> gids(numGids);
+	uncompress((Bytef*)&gids[0], &numGids, (const Bytef*)decodedIDs.c_str(), decodedIDs.size());
+
+	vector<int> layerRow(m_width);
+
+	for (int j = 0; j < m_height; ++j)
+	{
+		data.push_back(layerRow);
+	}
+
+	for (int rows = 0; rows < m_height; ++rows)
+	{
+		for (int cols = 0; cols < m_width; ++cols)
+		{
+			data[rows][cols] = gids[rows * m_width + cols];
+		}
+	}
+
+	pTileLayer->setTileIDs(data);
+
+	pLayers->push_back(pTileLayer);
 }*/
